5617083fa8000ca95710144c2bd014c0491b0f8a
Carlos <carlos@carlos>
2023-03-09 08:19:20 +0100

    [MOD] Cambios BBDD añadido precioTotal para facturas

--- Mysql/bbdd.sql
+++ Mysql/bbdd.sql
@@ -193,6 +193,7 @@ CREATE TABLE `facturaheader` (
   `servicio` int DEFAULT NULL,
   `cliente` int DEFAULT NULL,
   `tipo` int DEFAULT NULL,
+  `precio` double DEFAULT NULL,
   PRIMARY KEY (`idfactura`),
   KEY `fk2facturaheader_idx` (`cliente`),
   KEY `fk2facturaheader_idx1` (`tipo`),
@@ -207,7 +208,7 @@ CREATE TABLE `facturaheader` (
 
 LOCK TABLES `facturaheader` WRITE;
 /*!40000 ALTER TABLE `facturaheader` DISABLE KEYS */;
-INSERT INTO `facturaheader` VALUES (1,1,1,1),(2,1,1,1),(3,1,1,1);
+INSERT INTO `facturaheader` VALUES (1,1,1,1,0),(2,1,1,1,0),(3,1,1,1,0);
 /*!40000 ALTER TABLE `facturaheader` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -357,4 +358,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-07  6:32:13
+-- Dump completed on 2023-03-09  8:17:30
